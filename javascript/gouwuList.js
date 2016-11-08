e8f10c5ed6385d0e3da61b542c4719349e5aa528
j <xxx@gmail.com>
2016-11-08 19:37:08 +0800

    modified 结算js

--- javascript/gouwuList.js
+++ javascript/gouwuList.js
@@ -103,4 +103,170 @@ sousuoimg.onclick = function(){
   menu_ul.style.display = "none";
   input_div.style.display = "block";
 }
-
+var shoes1_div = document.getElementById("shoes1_div");
+var shoes2_div = document.getElementById("shoes2_div");
+var shoes3_div = document.getElementById("shoes3_div");
+var shoes4_div = document.getElementById("shoes4_div");
+var shoes5_div = document.getElementById("shoes5_div");
+var shoes6_div = document.getElementById("shoes6_div");
+var shoes7_div = document.getElementById("shoes7_div");
+var shoes8_div = document.getElementById("shoes8_div");
+var shoes9_div = document.getElementById("shoes9_div");
+var shoes10_div = document.getElementById("shoes10_div");
+var shoes11_div = document.getElementById("shoes11_div");
+var shoes12_div = document.getElementById("shoes12_div");
+var shoes13_div = document.getElementById("shoes13_div");
+var shoes14_div = document.getElementById("shoes14_div");
+var shoes15_div = document.getElementById("shoes15_div");
+var shoes16_div = document.getElementById("shoes16_div");
+var shoes17_div = document.getElementById("shoes17_div");
+var shoes18_div = document.getElementById("shoes18_div");
+var shoes19_div = document.getElementById("shoes19_div");
+var shoes20_div = document.getElementById("shoes21_div");
+var shoes22_div = document.getElementById("shoes22_div");
+var shoes23_div = document.getElementById("shoes23_div");
+var shoes24_div = document.getElementById("shoes24_div");
+shoes1_div.onmouseover = function(){
+  shoes1_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes1_div.onmouseout = function(){
+  shoes1_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes2_div.onmouseover = function(){
+  shoes2_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes2_div.onmouseout = function(){
+  shoes2_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes3_div.onmouseover = function(){
+  shoes3_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes3_div.onmouseout = function(){
+  shoes3_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes4_div.onmouseover = function(){
+  shoes4_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes4_div.onmouseout = function(){
+  shoes4_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes5_div.onmouseover = function(){
+  shoes5_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes5_div.onmouseout = function(){
+  shoes5_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes6_div.onmouseover = function(){
+  shoes6_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes6_div.onmouseout = function(){
+  shoes6_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes7_div.onmouseover = function(){
+  shoes7_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes7_div.onmouseout = function(){
+  shoes7_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes8_div.onmouseover = function(){
+  shoes8_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes8_div.onmouseout = function(){
+  shoes8_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes9_div.onmouseover = function(){
+  shoes9_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes9_div.onmouseout = function(){
+  shoes9_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes10_div.onmouseover = function(){
+  shoes10_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes10_div.onmouseout = function(){
+  shoes10_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes11_div.onmouseover = function(){
+  shoes11_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes11_div.onmouseout = function(){
+  shoes11_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes12_div.onmouseover = function(){
+  shoes12_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes12_div.onmouseout = function(){
+  shoes12_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes13_div.onmouseover = function(){
+  shoes13_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes13_div.onmouseout = function(){
+  shoes13_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes14_div.onmouseover = function(){
+  shoes14_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes14_div.onmouseout = function(){
+  shoes14_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes15_div.onmouseover = function(){
+  shoes15_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes15_div.onmouseout = function(){
+  shoes15_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes16_div.onmouseover = function(){
+  shoes16_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes16_div.onmouseout = function(){
+  shoes16_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes17_div.onmouseover = function(){
+  shoes17_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes17_div.onmouseout = function(){
+  shoes17_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes18_div.onmouseover = function(){
+  shoes18_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes18_div.onmouseout = function(){
+  shoes18_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes19_div.onmouseover = function(){
+  shoes19_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes19_div.onmouseout = function(){
+  shoes19_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes20_div.onmouseover = function(){
+  shoes20_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes20_div.onmouseout = function(){
+  shoes20_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes21_div.onmouseover = function(){
+  shoes21_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes21_div.onmouseout = function(){
+  shoes21_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes22_div.onmouseover = function(){
+  shoes22_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes22_div.onmouseout = function(){
+  shoes22_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes23_div.onmouseover = function(){
+  shoes23_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes23_div.onmouseout = function(){
+  shoes23_div.style.border = "1px solid rgb(221,227,228)"
+}
+shoes24_div.onmouseover = function(){
+  shoes24_div.style.border = "1px solid rgb(0,170,232)"
+}
+shoes24_div.onmouseout = function(){
+  shoes24_div.style.border = "1px solid rgb(221,227,228)"
+}
---
+++ javascript/zc.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2016/11/8.
+ */
+/**
+ * Created by Administrator on 2016/11/7.
+ */
+var zc_span = document.getElementById("zc_span");
+var zc_div = document.getElementById("zc_div");
+var z_div = document.getElementById("z_div");
+var zc_Dianji = document.getElementById("zc_Dianji");
+var ZDiv = document.getElementById("ZDiv");
+//zc_span.onclick = function(){
+//  zc_div.style.display = "block";
+//  z_div.style.display = "block";
+//}
+zc_Dianji.onclick = function(){
+  zc_div.style.display = "none";
+  ZDiv.style.display = "block";
+  //z_div.style.display = "none";
+}
+
